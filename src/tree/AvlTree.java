aa7ba2de6bbabaf4ca4bfd7802384d3235670bb2
harrily <17603018123@163.com>
2022-10-26 23:31:01 +0800

    [[

--- src/tree/AvlTree.java
+++ src/tree/AvlTree.java
@@ -137,19 +137,34 @@ public class AvlTree {
 	 */
 	public Node insert (int x) {
 		Node current = node;
-		if(node == null) { node = new Node(x); return node;}
+		if(node == null) { 
+			node = new Node(x);
+			node.height=Math.max(getHeight(node.left),getHeight(node.right))+1;//不要写成递归， 递归效率低
+			System.out.println("node.height000:"+node.height + " value : "+node.value);
+			return node;
+			}
 		while(current != null) {
 			if(x < current.value) {
 				if(current.left == null) {
-					return  current.left =  new Node(x);
+					current.left =  new Node(x);
+					current.height=Math.max(getHeight(current.left),getHeight(current.right))+1;//不要写成递归， 递归效率低
+					System.out.println("current.height111:"+current.height + " value : "+current.value);
+					return  current.left;
 				}else {
+					current.height=Math.max(getHeight(current.left),getHeight(current.right))+1;//不要写成递归， 递归效率低
+					System.out.println("current.height222:"+current.height+ " value : "+current.value);
 					current = current.left;
 				}
 			}
 			if(x > current.value) {
 				if(current.right == null) {
-					return  current.right =  new Node(x);
+					current.right =  new Node(x);
+					current.height=Math.max(getHeight(current.left),getHeight(current.right))+1;//不要写成递归， 递归效率低
+					System.out.println("current.height333:"+current.height+ " value : "+current.value);
+					return  current.right;
 				}else {
+					current.height=Math.max(getHeight(current.left),getHeight(current.right))+1;//不要写成递归， 递归效率低
+					System.out.println("current.height444:"+current.height+ " value : "+current.value);
 					current = current.right;
 				}
 			}
@@ -157,11 +172,96 @@ public class AvlTree {
 		
 		return current;
 	}
+	//  递归插入
+	public Node insert1(int value) {
+		return node = insert(value, node);
+    }
+	//实现
+	public Node insert(int x,Node t)//插入   t是root的引用
+    {
+		Node a1=new Node(x);
+        //if(root==null) {root=a1;return root;}		
+        if(t==null)    {return a1;}
+        //插入操作。递归实现
+        else if(t!=null)
+        {
+            if(x<t.value)
+            { t.left=insert(x,t.left);}
+            else
+            { t.right= insert(x,t.right);}
+        }
+        /*
+         * 更新当前节点的高度，因为整个插入只有被插入的一方有影响，
+         * 所以递归会更新好最底层的，上层可直接调用
+         */
+        t.height=Math.max(getHeight(t.left),getHeight(t.right))+1;//不要写成递归， 递归效率低
+        System.out.println("t高度:"+t.height+ " t is value : "+t.value);
+        return t;//因为java对象传参机制，需要克隆，不可直接t=xx 否则变换	  
+    }
 	
-	
+	public List<Integer> MiddleSearch(Node node,List<Integer> list,int flag){
+		if(node == null){
+            return list;
+        }
+		if(flag == 1 ) {
+			list.add(node.value);  // 前序
+			MiddleSearch(node.left, list,flag);
+			MiddleSearch(node.right, list,flag);
+		}
+		if(flag == 2 ) {
+			
+			MiddleSearch(node.left, list,flag);
+			list.add(node.value);  // 中序
+			MiddleSearch(node.right, list,flag);
+		}
+		if(flag == 3 ) {
+			MiddleSearch(node.left, list,flag);
+			MiddleSearch(node.right, list,flag);
+			list.add(node.value);  // 后序
+		}
+		
+		
+		return list;
+	}
 	
 	
 	public static void main(String[] args) {
+		AvlTree avlTree = new AvlTree();
+//		 Node node = avlTree.insert(15);
+//		 avlTree.insert(6);
+//		 avlTree.insert(23);
+//		 avlTree.insert(4);
+//		 avlTree.insert(7);
+//		 avlTree.insert(5);
+//		 avlTree.insert(19);
+//		 avlTree.insert(17);
+////		 treeTest.insert(21);
+//		 avlTree.insert(71);
+//		 avlTree.insert(50);
+//		 avlTree.insert(75);
 		
+		 /**
+		  * 				     15
+		  * 			6                 23
+		  * 		4      7		 19        71
+		  *            5          17          50	  75      
+		  * 
+		  */
+		
+		Node insert1 = avlTree.insert1(15);
+		 avlTree.insert1(6);
+		 avlTree.insert1(23);
+		 avlTree.insert1(4);
+		 avlTree.insert1(7);
+		 avlTree.insert1(5);
+		 avlTree.insert1(19);
+		 avlTree.insert1(17);
+//		 treeTest.insert(21);
+		 avlTree.insert1(71);
+		 avlTree.insert1(50);
+		 avlTree.insert1(75);
+		
+		 List<Integer> list = new ArrayList<Integer>();
+		 System.out.println(avlTree.MiddleSearch(insert1, list, 1));
 	}
 }
