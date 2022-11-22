d78910b4cb4d389b97d5631e30f30c2ebc3d095f
harrily <17603018123@163.com>
2022-11-23 00:00:32 +0800

    skip B+ summary 10%

--- src/tree/SkipList.java
+++ src/tree/SkipList.java
@@ -5,6 +5,10 @@ import java.util.Stack;
 
 /**
  * 跳跃表
+ * 
+ * 	跳表(SkipList，全称跳跃表)是用于有序元素序列快速搜索查找的一个数据结构，跳表是一个随机化的数据结构，
+ *      实质就是一种可以进行二分查找的有序链表。跳表在原有的有序链表上面增加了多级索引，通过索引来实现快速查找。
+ * 	跳表不仅能提高搜索性能，同时也可以提高插入和删除操作的性能。它在性能上和红黑树，AVL树不相上下，但是跳表的原理非常简单，实现也比红黑树简单很多。
  * 参考资料： https://blog.csdn.net/qq_40693171/article/details/111751992
  * @author wang.ying.nan
  *
@@ -13,7 +17,7 @@ class SkipNode<T>
 {
     int key;
     T value;
-    SkipNode right,down;//左右上下四个方向的指针
+    SkipNode right,down;//维护右下两个方向的指针
     public SkipNode (int key,T value) {
         this.key=key;
         this.value=value;
