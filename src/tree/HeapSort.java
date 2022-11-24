816aff1e293fb4ae29b07f8cf078a3d8986ddc6e
harrily <17603018123@163.com>
2022-11-24 23:35:46 +0800

    heapSort zice

--- src/tree/HeapSort.java
+++ src/tree/HeapSort.java
@@ -1,28 +1,22 @@
 package tree;
 
+import java.util.Arrays;
 import java.util.List;
 
 /**
  * 
  * 堆排序
+ * 
+1、 将待排序序列构造成一个大顶堆
+2、此时，整个序列的最大值就是堆顶的根节点。
+3、将其与末尾元素进行交换，此时末尾就为最大值。
+4、然后将剩余n-1个元素重新构造成一个堆，这样会得到n个元素的次小值。如此反复执行，便能得到一个有序序列了。
  * 参考资料：https://blog.csdn.net/weixin_51609435/article/details/122982075
  * @author wang.ying.nan
  *
  */
 public class HeapSort {
 
-	class Node{
-		Node left ;
-		Node right;
-		Node parent;
-		int key;
-		public Node(int key,Node left ,Node right) {
-			// TODO Auto-generated constructor stub
-			this.left = left;
-			this.right = right;
-			this.key = key;
-		}
-	}
 	/**
 	 * 完全二叉树
 			  	           0
@@ -58,6 +52,8 @@ public class HeapSort {
 		int temp;
 		int length = arr.length;
 		int i = arr.length/2 -1 ;
+		int times  = 0;
+		// 上溯至root构造最大堆
 		while(i >= 0 ) {
 			for(int j = i ; j >=0; j--) {
 	//			System.out.println(j);
@@ -79,6 +75,7 @@ public class HeapSort {
 					arr[index] = arr[j];
 					arr[j] = temp;
 				}
+				times++;
 			}
 			// 堆顶与末尾元素互换
 			temp = arr[0];
@@ -88,33 +85,113 @@ public class HeapSort {
 			length--;
 			// 更新i值
 			i = length/2 -1 ;
-//			System.out.println(length + "==" + i);
+			System.out.println(Arrays.toString(arr));
 		}
+		System.out.println( "times +==" + times);
 		return arr;
 	}
-		
+	
+	
+	/**
+	 * 
+	 *   demo
+	 *   	每次构建最大堆， 除了root是最大， 其他有孩子的node也要是最大的值、；
+	 * @param arr
+	 */
+	 public static void heapSort(int[] arr){
+		  int times = 0;
+	        //给你一个数组 {4,6,8,5,9} , 要求使用堆排序法，将数组升序排序
+	        /*
+	         将待排序序列构造成一个大顶堆
+	        此时，整个序列的最大值就是堆顶的根节点。*/
+	        /*adjustHeap( arr,1,arr.length);
+	        adjustHeap( arr,0,arr.length);*/
+	        for (int i = arr.length/2-1; i >=0; i--) {
+	            times = adjustHeap( arr,i,arr.length,times);
+	        }
+	        int temp=0;
+	        /*
+	         将其与末尾元素进行交换，此时末尾就为最大值。
+	         然后将剩余n-1个元素重新构造成一个堆，这样会得到n个元素的次小值。如此反复执行，便能得到一个有序序列了。*/
+	        for (int j=arr.length-1;j>0;j--){
+	            temp=arr[j];
+	            arr[j]=arr[0];
+	            arr[0]=temp;
+	            times = adjustHeap(arr,0,j,times);
+	        }
+	        System.out.println("tttimes:"+times);
+	    }
+	    public static int adjustHeap(int[] arr,int i,int length,int times){
+	        //第一步把数组 {4,6,8,5,9}调整排序为{4,9,8,5,6}
+	        //k=i*2+1 是循环对应的左子树元素
+	        //记录当前要调整的元素
+	        int temp=arr[i];
+	        for (int k=i*2+1;k<length;k=k*2+1){
+	            //把当前左子树和右子树的数据作比较，更换较大的数据
+	            if (k+1<length&&arr[k]<arr[k+1]){
+	                k++;
+	            }
+	            if (arr[k]>temp){
+	                //交换位置
+	                arr[i]=arr[k];
+	                i=k;
+	            }else{
+	                break;
+	            }
+	            times++;
+	        }
+	         arr[i]=temp;
+	         return times;
+	    }
+	    
+	    
 	public static void main(String[] args) {
+		int [] arr = new int[] {4,6,8,5,9,11};
+//	    heapSort(arr); 
+//	    System.out.println(Arrays.toString(arr)); 
+	    System.out.println( Arrays.toString(new HeapSort().sort(arr)));
+		/**
+		 * 
+		 int [] arr = new int[10000];
+		 int i = 10000;
+		 for(int j = 0 ;j<10000; j++) {
+			 arr[j] = i;
+			 i-- ;
+		 }
+// 		demo   10000数据，循环  tttimes:106697
+//	    heapSort(arr); 
+//	    System.out.println(Arrays.toString(arr));   
+		
+		// 自实现 10000数据，循环   times +==25000000
+//	    System.out.println( Arrays.toString(new HeapSort().sort(arr)));
+		 */
+		
+		
 //		int [] arr = new int[] {4,6,3,5,9,7,123,45,124,2322,35,23,2,-1};
 //		int[] sort = new HeapSort().sort(arr);
 //		for(int i : sort) {
 //			System.out.println(i);
 //		}
 		
-		
-		 int [] test = new int[1000];
-		 for(int j = 0 ;j<100; j++) {
-			 test[j] = (int)(Math.random()*100-1);;
-		 }
-		 int[] sort2 = new HeapSort().sort(test);
-		 for(int i1 : sort2) {
-				System.out.println(i1);
-			}
+		 /**
+		  * 
+		  * 随机数测试
+		  */
+//		 int [] test = new int[1000];
+//		 for(int j = 0 ;j<100; j++) {
+//			 test[j] = (int)(Math.random()*100-1);;
+//		 }
+//		 int[] sort2 = new HeapSort().sort(test);
+//		 for(int i1 : sort2) {
+//				System.out.println(i1);
+//			}
+		 
 		/**
 		 int i = 1000;
 		 int [] test = new int[1000];
 		 for(int j = 0 ;j<1000; j++) {
-			 test[j] = i;
-			 i-- ;
+			 test[j] = j;
+			 j-- ;
 		 }
 		 int[] sort2 = new HeapSort().sort(test);
 		 for(int i1 : sort2) {
@@ -123,4 +200,7 @@ public class HeapSort {
 		**/
 		
 	}
+	
+	
+	    
 }
